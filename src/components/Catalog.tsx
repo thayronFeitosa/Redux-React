52d1f384d86165a0aa2e3553788df342d917a833
thayron <thayronf3@gmail.com>
2021-01-09 16:03:27 -0300

    Listando os valores da api

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import {Iproduct} from '../store/modules/cart/types';
+import api from '../services/api';
 
 const Catalog: React.FC = () => {
-    const catalog = useSelector(state => state);
-    console.log(catalog);
-    
+    const [catalog, setCatalog] = useState<Iproduct[]>([])
+    useEffect(() =>{
+        api.get('products').then(response =>{
+            setCatalog(response.data);
+        })
+    }, [])
     return (
-        <h1>Catalog</h1>
+        <main>
+            <h1>Catalog</h1>
+
+            {catalog.map(product =>(
+                <article key={product.id}>
+                    <strong>{product.title}</strong> {" - "}
+                    <span>{product.price}</span>{" "}
+                    <button type="button">Comprar</button>
+                </article>
+            ))}
+        </main>
     );
 };
 
