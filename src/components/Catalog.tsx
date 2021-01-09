32575566810100dbbbc428535dd0517328f2ca7c
thayron <thayronf3@gmail.com>
2021-01-09 09:51:46 -0300

    Foi utilizado o useSelector para listar os dados do store

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,20 +1,11 @@
-import { useStore } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const Catalog: React.FC = () => {
-    const store = useStore();
-    const { id, name, email } = store.getState();
-
-    return (
-        <>
-        <h1>Olá { name } seus dados seguem a baixo</h1>
-        <ul>
-            <li>Id: {id} </li>  
-            <li>Nome: { name }</li>
-            <li>Email: { email }</li>
-        </ul>
-        </>
-        
+    const store = useSelector(state => state);
+    console.log(store);
     
+    return (
+        <h1>Catalog</h1>
     );
 };
 
