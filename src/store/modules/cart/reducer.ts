f78b9f3be6ef6c1f4e8ca5a07663f7764c562e68
thayron <thayronf3@gmail.com>
2021-01-10 12:05:35 -0300

    FOi utilizado a biblioteca immer para retirar a verbosidade do reducer escrevendo o algoritimo de forma muito mais simples ao ultilizar imutanilidade

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,30 +1,29 @@
 import { Reducer } from 'redux';
 import { IcartState } from './types';
+import produce from 'immer';
 
 const INITIAL_STATE: IcartState = {
     items: []
 };
 
 export const cart: Reducer<IcartState> = (state = INITIAL_STATE, action) =>{
-    switch(action.type){
-        case 'ADD_PRODUCT_TO_CART': {
-            const { product } = action.payload;
-            return { 
-             ...state,
-             items:[
-                 ...state.items,
-                 {
+    return produce(state, draft => {
+        switch(action.type){
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload;
+
+                draft.items.push({
                     product,
-                    quantity: 1
-                 }
-             ]
-            };
-            
+                    quantity: 1,
+                });
+                
+                break;      
+            }
+            default: {
+                return draft;
+            }
         }
-        default: {
-            return state;
-        }
-    }
+    });
 }
 
 export const loja: Reducer<IcartState> = () =>{
