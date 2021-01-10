3efd76bdc332013a5edd8d6791536b5e83869cfc
thayron <thayronf3@gmail.com>
2021-01-10 11:42:35 -0300

    Foi criado o swhit e usado o conceito de imutabilidade para adicionar um novo produto no carrinho de comparas. Foi listado tudo em um console log dentro do componente Cars

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,7 +1,6 @@
 import { Iproduct } from './types';
 
 export function addProductToCart(product: Iproduct){
-
     return{
         type: 'ADD_PRODUCT_TO_CART',
         payload:{
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,10 +5,26 @@ const INITIAL_STATE: IcartState = {
     items: []
 };
 
-export const cart: Reducer<IcartState> = (state, action) =>{
-    console.log(state, action);
-    
-    return INITIAL_STATE;
+export const cart: Reducer<IcartState> = (state = INITIAL_STATE, action) =>{
+    switch(action.type){
+        case 'ADD_PRODUCT_TO_CART': {
+            const { product } = action.payload;
+            return { 
+             ...state,
+             items:[
+                 ...state.items,
+                 {
+                    product,
+                    quantity: 1
+                 }
+             ]
+            };
+            
+        }
+        default: {
+            return state;
+        }
+    }
 }
 
 export const loja: Reducer<IcartState> = () =>{
